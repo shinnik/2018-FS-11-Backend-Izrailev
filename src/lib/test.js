cfea7bc0e1af67d9a498f5a6e119e208858638b6
Nikita Izrailev <izrailev@phystech.edu>
2018-10-11 18:36:05 +0300

    css is not in js code

--- src/lib/test.js
+++ src/lib/test.js
@@ -3,16 +3,7 @@ export default function say(name) {
   let parentBlock;
 
   childBlock = document.createElement('div');
-  childBlock.style.background = 'Aquamarine';
-  childBlock.style.textAlign = 'center';
-  childBlock.style.margin = 'Auto';
-  childBlock.style.marginTop = '250px';
-  childBlock.style.width = '200px';
-  // childBlock.style.height = "100px";
-  childBlock.style.padding = '40px';
-  // childBlock.style.marginLeft = "500px";
-  // childBlock.style.marginTop = "300px";
-  childBlock.style.border = '5px solid lightblue';
+  childBlock.className = "dif_block";
 
   childBlock.innerHTML = `Hello, ${name}!`;
 

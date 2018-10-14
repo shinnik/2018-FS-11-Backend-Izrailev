d11804643e37fa1414337fe907a067b118dfe7ba
Nikita Izrailev <izrailev@phystech.edu>
2018-10-14 20:37:34 +0300

    Eslint does not insult me anymore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import say from './lib/test.js';
+import say from './lib/test';
 
 window.onload = function () {
   say('world');
--- src/lib/test.js
+++ src/lib/test.js
@@ -1,13 +1,10 @@
 export default function say(name) {
-  let childBlock;
-  let parentBlock;
-
-  childBlock = document.createElement('div');
-  childBlock.className = "dif_block";
+  const childBlock = document.createElement('div');
+  childBlock.className = 'dif_block';
 
   childBlock.innerHTML = `Hello, ${name}!`;
 
-  parentBlock = document.body;
+  const parentBlock = document.body;
   parentBlock.appendChild(childBlock);
 
   childBlock.id = 'another_background';

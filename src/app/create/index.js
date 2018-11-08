0abab6cc28190a59ff313bd8911ed2671bbbb883
Nikita Izrailev <izrailev@phystech.edu>
2018-11-08 12:26:31 +0300

    master clean

--- src/app/create/index.js
+++
@@ -1,4 +0,0 @@
-import FormInput from '../../lib/components/form/-input';
-import MessageForm from '../../lib/components/message-form';
-import GeoForm from '../../lib/components/message-form/geo-form/geo-form';
-import DragAndDropZone from '../../lib/components/drag-n-drop/drag-n-drop';

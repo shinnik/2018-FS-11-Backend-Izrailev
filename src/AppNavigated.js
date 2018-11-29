bf9ce6d94af1f8104f81b256bb43df67736e779b
Nikita Izrailev <izrailev@phystech.edu>
2018-11-29 11:40:06 +0300

    extra comment deleted

--- src/AppNavigated.js
+++ src/AppNavigated.js
@@ -19,8 +19,6 @@ class App extends Component {
           <Layout>
               <Route path='/list_chats/chat_id=1' component={ () => <MessageForm id="1" /> } />
               <Route path='/list_chats/chat_id=2' component={ () => <MessageForm id="2" /> } />
-              <Route path='/list_chats/chat_id=3' component={ () => <MessageForm id="3" /> } />
-              <Route path='/list_chats/chat_id=4' component={ () => <MessageForm id="4" /> } />
               <Route path='/list_chats' exact component={ () => <Chats />} />
               <Route path='/main' exact component={Main} />
 	      <Route path='/' exact component={Main} />
@@ -29,18 +27,6 @@ class App extends Component {
     );
   }
 }
-
-
-//class App extends Component {
- // render() {
- //   return (
-  //      <div>
-  //        <div class="head"></div>
-  //        <MessageForm></MessageForm>
-  //      </div>
-  //  );
- // }
-//}
 
 
 export default App;

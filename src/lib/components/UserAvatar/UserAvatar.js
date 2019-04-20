cee66d9890578efd41ac72815e99d208fd1d0922
Nikita Izrailev <izrailev@phystech.edu>
2019-04-20 11:02:33 +0300

    small refactoring

--- src/lib/components/UserAvatar/UserAvatar.js
+++ src/lib/components/UserAvatar/UserAvatar.js
@@ -5,23 +5,18 @@ import ReactDOM from 'react-dom';
 
 export const UserAvatar = () => {
 
-    const shadowStyle = {
-        zIndex: 3,
-        position: 'fixed',
-        height: '100%',
-        width: '100%',
-        backgroundColor: '#222f3e',
-        opacity: 0.5,
-        top: 0,
-};
 
     const [clicked, setClicked] = useState(false);
-    const shad = <div style={shadowStyle}></div>;
+    const shad = <div className={classes.shadowStyle}></div>;
 
     return (
         <div>
             {clicked && <div>{ReactDOM.createPortal(shad, document.body)}</div>}
-            <img onClick={() => setClicked(!clicked)} alt="noav" src={'https://api.adorable.io/avatars/220/abott@adorable.png'} className={clicked ? classes.avatarSelected : classes.avatar}></img>
+            <img onClick={() => setClicked(!clicked)}
+                 alt="noav"
+                 src={'https://api.adorable.io/avatars/220/abott@adorable.png'}
+                 className={clicked ? classes.avatarSelected : classes.avatar}>
+            </img>
         </div>
 
     )
